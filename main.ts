9552b51720e2e455931d1f166cfa18d38b64275d
cakepower <32028127+cakepower@users.noreply.github.com>
2022-11-17 10:34:22 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,15 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 50; index++) {
-        for (let index2 = 0; index2 <= 7; index2++) {
-            strip.setPixelColor(index2, neopixel.colors(NeoPixelColors.Green))
-            strip.show()
-            basic.pause(100)
-        }
-        for (let index3 = 0; index3 <= 7; index3++) {
-            strip.setPixelColor(index3, neopixel.colors(NeoPixelColors.Black))
-            strip.show()
-            basic.pause(100)
-        }
+    for (let index2 = 0; index2 <= 7; index2++) {
+        strip.setPixelColor(index2, neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+        basic.pause(100)
+    }
+    for (let index3 = 0; index3 <= 7; index3++) {
+        strip.setPixelColor(index3, neopixel.colors(NeoPixelColors.Black))
+        strip.show()
+        basic.pause(100)
     }
 })
 input.onButtonPressed(Button.B, function () {
